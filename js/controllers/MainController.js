afe9fc82161d960f47ee4ef42958a4d423aecd62
Venkatesh MA <geekvenk@yahoo.com>
2015-06-11 21:54:29 +0530

    Add, Edit & Delete List
    Switched from Jquery approach to Ang JS for show/hide features.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -59,11 +59,11 @@ app.controller('MainController', ['$scope', function($scope) {
       cardid:'2',
       content: 'Glyphicons Halflings are normally not available for free',
       pubdate: new Date('2015', '08', '01')
-      }, 
+      },
       { 
-      cardid:'3',
-      content: 'Includes over 250 glyphs in font format from the Glyphicon Halflings set.',
-      pubdate: new Date('2015', '08', '01')
+      cardid:'1',
+      content: 'Includes over 250 glyphs in font format from the Glyphicon Halflings set.', 
+      pubdate: new Date('2015', '03', '08')
       }]
   	}, 
   	{ 
@@ -82,27 +82,47 @@ app.controller('MainController', ['$scope', function($scope) {
       }]
   	}
   ];
-    
+    var i,ii;
+    var tempList;
+    $scope.newList='';
+    $scope.newListBool=true;
     $scope.newCardContent=new Array();
     $scope.addCardIndex=new Array();
-    
     $scope.editCardContent=new Array();
     $scope.editCardIndex=new Array();
+    $scope.editList=new Array();
+    $scope.editListIndex=new Array();
+    $scope.cardTitleHover=new Array();
+    $scope.editListContent=new Array();
+    $scope.cardHover=new Array();
     $scope.currentAddCardClass='.addCard0';
     $scope.hiddenAddCardHiddenClass='.addCardHidden0';
-    $scope.cardHoverIn = function(index) { 
-      $(index).css("display","block");
-      console.log("Hovering card ->"+index);
-    };
-    $scope.cardHoverOut = function(index) {
-      $(index).css("display","none");
-      console.log("Hover Out card ->"+index);
+
+    $scope.initiate=function(){
+      i=0;
+    $scope.lists.forEach(function(list) {
+      $scope.addCardIndex[i]=true;
+      $scope.editListIndex[i]=true;
+      $scope.cardTitleHover[i]=true;
+      $scope.editList[i]='';
+      $scope.editListContent[i]=list.name;
+      $scope.cardHover[i]=new Array();
+      $scope.editCardContent[i]=new Array();
+      $scope.editCardIndex[i]=new Array();
+      ii=0;
+      list.cards.forEach(function(card) {
+        $scope.editCardIndex[i][ii]=true;
+        $scope.cardHover[i][ii]=true;
+        $scope.editCardContent[i][ii]=card.content;
+        ii++;
+      });
+      i++;
+    });
     };
 
+    $scope.initiate();
     $scope.editCard = function(parentIndex,index) {
-      //cardId='#'+parentIndex+'cardContent'+index;
       $scope.editCardIndex[parentIndex][index]=false;
-      //$(cardId).css("display","none");
       console.log("editCardContent ->"+$scope.editCardIndex[parentIndex][index]);
       console.log("Hover Out card ->"+index);
     };
@@ -124,7 +144,7 @@ app.controller('MainController', ['$scope', function($scope) {
     };
 
     $scope.addCardProcess = function(index) { 
-    
+
     $scope.addCardIndex[index]=false;
     console.log("Card adding...");
     };
@@ -145,8 +165,58 @@ app.controller('MainController', ['$scope', function($scope) {
 
     $scope.newCardContent[index]="";
     $scope.addCardIndex[index]=true;
-  };
+    };
 
-console.log($scope.editCardContent);
+    $scope.addList = function(name) { 
+    $scope.newListBool=false;
+    tempList={ 
+      id:'2',
+      name: name, 
+      pubdate: new Date('2015', '03', '08'),
+      cards:[{
+      cardid:'2',
+      content: 'Sample Card. Cheers!',
+      pubdate: new Date('2015', '08', '01')
+      }]
+    };
+    console.log("List name ="+name);
+    console.log("tempList ="+tempList);
+    $scope.lists.push(tempList);
+    console.log("Adding List...");
+    console.log("Added Card -> "+tempList.name);
+    $scope.newList="";
+    $scope.newListBool=true;
+    $scope.initiate();
+    };
+
+    $scope.addNewListUI = function() { 
+    $scope.newListBool=false;
+    };
+
+    $scope.saveList = function(index,conten) { 
+    console.log("Save index "+index+"\n"+"conten "+conten);
+    $scope.lists[index].name=conten;
+    console.log("Stored "+$scope.lists[index].name);
+    console.log("indexbool "+$scope.editListIndex[index]);
+    $scope.editListIndex[index]=true;
+    $scope.editList[index]='';
+    $scope.initiate();
+    };
+
+    $scope.cancelList = function() { 
+    $scope.newListBool=true;
+    };
+
+    $scope.cancelSaveList = function(index) { 
+    $scope.editListIndex[index]=true;
+    };
+
+    $scope.deleteList = function(index) {
+      $scope.lists.splice(index, 1);
+      $scope.initiate();
+      console.log("List Deleted->"+index);
+    };
+
+
 
 }]);
